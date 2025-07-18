abc451d836308cf614e439a385025979c6f02fed
gg3333ofk <dragonwarhd@gmail.com>
2025-07-18 16:14:55 +0700

    Update poopgame.lua

--- poopgame.lua
+++ poopgame.lua
@@ -44,7 +44,7 @@ Rayfield:Notify({
 },
 })
 local Toggle = Tab:CreateToggle({
-   Name = "RapidPoops Exploit(Optimized)",
+   Name = "RapidPoops Exploit(Optimized) - FIXED :(",
    CurrentValue = false,
    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
