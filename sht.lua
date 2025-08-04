7a0784889075e5db78e180b3f84594a493b09555
wanna_exploits <dragonwarhd@gmail.com>
2025-08-04 19:57:29 +0700

    Update sht.lua

--- sht.lua
+++ sht.lua
@@ -1,45 +1,14 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local Window = Rayfield:CreateWindow({
-   Name = "Super Hero Tycoon Scripted",
-   LoadingTitle = "Rayfield",
-   LoadingSubtitle = "by gg3333",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "super hero tycoon scripted"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
-local prevpos = nil
-local Tab = Window:CreateTab("Main")
-local Section = Tab:CreateSection("Farm")
-local Toggle = Tab:CreateToggle({
-   Name = "Auto Farm",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-    local PlayerRoot = game.Players.LocalPlayer.Character.HumanoidRootPart
-	val = Value
-	while val == true do
-		task.wait(0.1)
-		for _, i in pairs(workspace.CrateSpawns:GetChildren()) do
-			i.CFrame = PlayerRoot.CFrame
-		end
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
+local Window = Library.CreateLib("Super Hero Tycoon Scripted", "RJTheme3")
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Farming")
+local stt = nil
+Section:NewToggle("Auto Farm", "ToggleInfo", function(state)
+    local PlayerRoot = game.Players.LocalPlayer.Character.HumanoidRootPart
+	stt = state
+	while stt == true do
+		task.wait(0.1)
 		offf = workspace.CratesCache:GetChildren()
   		for _, i in pairs(offf) do
     		object = i
@@ -47,37 +16,19 @@ local Toggle = Tab:CreateToggle({
  		end
   		for _, i in pairs(object:GetChildren()) do
 			prevpos = i.Position
+            i.Transparency = 0
 			i.CanCollide = false
 			i.CFrame = PlayerRoot.CFrame
-    		task.wait(0.001)
+            task.wait(0.0000000000000001)
 			i.Position = prevpos
     	end
 	end
-   end,
-})
-local Section = Tab:CreateSection("Player")
+end)
 
-local Slider = Tab:CreateSlider({
-   Name = "Walk Speed",
-   Range = {10, 200},
-   Increment = 2,
-   Suffix = "Walkspeed",
-   CurrentValue = 20,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-   end,
-})
-local Slider = Tab:CreateSlider({
-   Name = "Jump Power",
-   Range = {10, 200},
-   Increment = 2,
-   Suffix = "Jump Power",
-   CurrentValue = 20,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-		while game:GetService("RunService").RenderStepped:Wait() do
-			game.Players.LocalPlayer.Character.Humanoid.JumpHeight = Value
-		end
-   end,
-})
+local Section = Tab:NewSection("Player")
+Section:NewSlider("WalkSpeed", "Player's Speed", 250, 1, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+Section:NewSlider("JumpPower", "Player's Jump Power", 250, 1, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.JumpHeight = s
+end)
