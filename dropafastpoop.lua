f21e3b853085eb55afb547cc538cf139f74760dc
wanna_exploits <dragonwarhd@gmail.com>
2025-08-12 11:16:21 +0700

    Create dropafastpoop.lua

---
+++ dropafastpoop.lua
@@ -0,0 +1,21 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+local Window = Library.CreateLib("Drop a poop [BETA]", "RJTheme5")
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Exploits/Farming")
+local stata = nil
+Section:NewToggle("Poop Exploit", "Fast poops", function(state)
+    stata = state
+    while stata == true and task.wait(0.0001) do
+        game.ReplicatedStorage.PoopChargeStart:FireServer()
+        game.ReplicatedStorage.PoopEvent:FireServer(1)
+        game.ReplicatedStorage.PoopChargeStart:FireServer()
+    end
+end)
+
+Section:NewToggle("Sell Exploit", "selling poop", function(state)
+    stata = state
+    while stata == true and task.wait(0.01) do
+        game.ReplicatedStorage.Sell_Inventory:FireServer()
+    end
+end)
+local Section = Tab:NewSection("Soon...")
