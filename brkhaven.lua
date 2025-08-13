f4def7b996b59d36e9235734047a349b43451263
wanna_exploits <dragonwarhd@gmail.com>
2025-08-13 22:08:42 +0700

    Update brkhaven.lua

--- brkhaven.lua
+++ brkhaven.lua
@@ -1,41 +1,89 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Brookhaven Scripted [Beta]", "RJTheme2")
-local Tab = Window:NewTab("Main")
+local MacLib = loadstring(game:HttpGet("https://github.com/biggaboy212/Maclib/releases/latest/download/maclib.txt"))()
+local Window = MacLib:Window({
+	Title = "Brookhaven Scripted",
+	Subtitle = "Version: Early Beta 1",
+	Size = UDim2.fromOffset(868, 650),
+	DragStyle = 1,
+	DisabledWindowControls = {},
+	ShowUserInfo = true,
+	Keybind = Enum.KeyCode.RightControl,
+	AcrylicBlur = true,
+})
+local tabGroups = {
+	TabGroup1 = Window:TabGroup()
+}
+local tabs = {
+	Main = tabGroups.TabGroup1:Tab({ Name = "Players"}),
+	Main2 = tabGroups.TabGroup1:Tab({ Name = "Tools"}),
+	
+}
+local sections = {
+	MainSection1 = tabs.Main:Section({ Side = "Left" }),
+	MainSection2 = tabs.Main2:Section({ Side = "Left" }),
+}
+
+sections.MainSection1:Header({
+	Name = "Auto Kill"
+})
+local plr = game.Players.LocalPlayer
 local plr3 = nil
 local plr2 = nil
-local Section = Tab:NewSection("Players")
+sections.MainSection1:Input({
+	Name = "User's name",
+	Placeholder = "Write username",
+	AcceptedCharacters = "All",
+	Callback = function(input)
+		plr3 = input
+	end,
+})
 
-if not game.Players.LocalPlayer.Backpack:FindFirstChild("Stretcher") then
-    game.ReplicatedStorage.RE["1Too1l"]:InvokeServer("PickingTools", "Stretcher")
-end
-Section:NewTextBox("Player's nickname(And press Enter)", "Lots of bugs", function(txt)
-    plr3 = txt
-end)
-Section:NewButton("Kill Player", "it may not work with people who have a weak device", function()
-    local plr = game.Players.LocalPlayer
-    local name = game.Players.LocalPlayer.Name
-    print(plr3)
-    plr2 = game.Players[plr3]
-    if workspace[name]:FindFirstChild("Stretcher") then
-        workspace[name].Stretcher.Parent = plr.Backpack
-    end
-    if plr.Backpack:FindFirstChild("Stretcher") then
-        plr.Backpack.Stretcher.Parent = workspace[name]
-        local prevpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-        wait(0.05)
-        for i = 0, 225 do
-            plr.Character.HumanoidRootPart.CFrame = plr2.Character.HumanoidRootPart.CFrame + (-plr2.Character.HumanoidRootPart.CFrame.LookVector * 4.1) + Vector3.new(0, -0.65, 0)
-            game:GetService("RunService").RenderStepped:Wait()
+sections.MainSection1:Button({
+	Name = "Get Couch",
+	Callback = function()
+        plr.Character.PrimaryPart.CFrame = workspace.WorkspaceCom["003_CouchGiveTool"].Main.CFrame + Vector3.new(0, 1, 0)
+	end,
+})
+sections.MainSection1:Button({
+	Name = "Kill Player(, Required Couch in inventory)",
+	Callback = function()
+        plr2 = game.Players[plr3]
+        local name = game.Players.LocalPlayer.Name
+        if workspace[name]:FindFirstChild("Couch") then
+            workspace[name].Couch.Parent = plr.Backpack
         end
-        for i = 0, 400 do
-            plr.Character.HumanoidRootPart.CFrame += Vector3.new(12.5, -0.25, 0)
-            game:GetService("RunService").RenderStepped:Wait()
+        if plr.Backpack:FindFirstChild("Couch") then
+            plr.Backpack.Couch.Parent = workspace[name]
+            local prevpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            task.wait(0.05)
+            for i = 0, 130 do
+                plr.Character.HumanoidRootPart.CFrame = plr2.Character.HumanoidRootPart.CFrame - Vector3.new(0, 1.5, 0)
+                task.wait()
+            end
+            for i = 0, 200 do
+                plr.Character.HumanoidRootPart.CFrame = plr.Character.HumanoidRootPart.CFrame + Vector3.new(45.5, 1.5, 0)
+                task.wait()
+            end
+            task.wait(0.80)
+            workspace[name].Couch.Parent = plr.Backpack
+            task.wait(0.3)
+            plr.Character.HumanoidRootPart.CFrame = prevpos
+            
         end
+	end,
+})
 
-        wait(0.80)
-        workspace[name].Stretcher.Parent = plr.Backpack
-        wait(0.5)
-        plr.Character.HumanoidRootPart.CFrame = prevpos
-    end
-
-end)
+sections.MainSection2:Button({
+	Name = "Equip All Tools(Requires open tools menu)",
+	Callback = function()
+        for _, i in pairs(game:GetService("Players")["33asdsaddfssdaadsd"].PlayerGui.NoResetGUIHandler.MainToolMenu.Catalog.Container.ScrollingFrame:GetChildren()) do
+            if i.Name ~= "UIGridLayout" then
+                game:GetService("ReplicatedStorage").RE["1Too1l"]:InvokeServer("PickingTools", i.Name)
+                task.wait()
+            end
+        end
+        for _, i in pairs(plr.Backpack:GetChildren()) do
+            i.Parent = workspace[plr.Name]
+        end
+        
+	end,
+})
