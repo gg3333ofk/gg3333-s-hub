92b0b0b25256ccd413894cfb7f8641bfebc018ec
wanna_exploits <dragonwarhd@gmail.com>
2025-08-13 13:59:52 +0700

    Update dropafastpoop.lua

--- dropafastpoop.lua
+++ dropafastpoop.lua
@@ -3,25 +3,39 @@ local Window = Library.CreateLib("Drop a poop [BETA]", "RJTheme5")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Exploits/Farming")
 local stata = nil
+local stata1 = nil
+local stata2 = nil
 
 Section:NewButton("Buy All", "buying luckyfood", function()
     for _, i in pairs(game.ReplicatedStorage.LuckyFoodModels:GetChildren()) do
         game.ReplicatedStorage.Network["LuckyFoodShop:RequestPurchase"]:InvokeServer(i.Name)
     end
 end)
-Section:NewToggle("Poop Exploit", "Fast poops", function(state)
-    stata = state
-    while stata == true and task.wait(0.0001) do
+Section:NewToggle("Poop Exploit [IN FIXING]", "Fast poops", function(state)
+    stata1 = state
+    while stata1 == true and task.wait(0.0001) do
         game.ReplicatedStorage.PoopChargeStart:FireServer()
-        game.ReplicatedStorage.PoopEvent:FireServer(1)
+        game.ReplicatedStorage.PoopEvent:FireServer(2)
         game.ReplicatedStorage.PoopChargeStart:FireServer()
     end
 end)
 
 Section:NewToggle("Sell Exploit", "selling poop", function(state)
-    stata = state
-    while stata == true and task.wait(0.01) do
+    stata2 = state
+    while stata2 == true and task.wait(0.01) do
         game.ReplicatedStorage.Sell_Inventory:FireServer()
     end
 end)
-local Section = Tab:NewSection("Soon...")
+local Section = Tab:NewSection("Viewer")
+local inpt = game:GetService("UserInputService")
+local prev = workspace.LuckyFoodShopNPC.Torso.ProximityPrompt.MaxActivationDistance
+Section:NewToggle("LuckyFood Shop Stock Viewer(Check info!!!!!)", "Press button 'E' 2 times", function(state)
+    stata = state
+    inpt.InputBegan:Connect(function(key)
+        if key.KeyCode == Enum.KeyCode.E and stata then
+            workspace.LuckyFoodShopNPC.Torso.ProximityPrompt.MaxActivationDistance = 1000000
+            task.wait(1.5)
+            workspace.LuckyFoodShopNPC.Torso.ProximityPrompt.MaxActivationDistance = prev
+        end
+    end)
+end)
