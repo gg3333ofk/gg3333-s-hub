84f1b288480fb5b41a1d3f5dd342dff70fe0edcd
wanna_exploits <dragonwarhd@gmail.com>
2025-08-14 14:10:29 +0700

    Update brkhaven.lua

--- brkhaven.lua
+++ brkhaven.lua
@@ -1,7 +1,7 @@
 local MacLib = loadstring(game:HttpGet("https://github.com/biggaboy212/Maclib/releases/latest/download/maclib.txt"))()
 local Window = MacLib:Window({
 	Title = "Brookhaven Scripted",
-	Subtitle = "Version: Early Beta 1",
+	Subtitle = "Version: Early Beta 2",
 	Size = UDim2.fromOffset(868, 650),
 	DragStyle = 1,
 	DisabledWindowControls = {},
@@ -15,15 +15,24 @@ local tabGroups = {
 local tabs = {
 	Main = tabGroups.TabGroup1:Tab({ Name = "Players"}),
 	Main2 = tabGroups.TabGroup1:Tab({ Name = "Tools"}),
+	Main3 = tabGroups.TabGroup1:Tab({ Name = "Local Player"}),
 	
 }
 local sections = {
 	MainSection1 = tabs.Main:Section({ Side = "Left" }),
 	MainSection2 = tabs.Main2:Section({ Side = "Left" }),
+	MainSection3 = tabs.Main3:Section({ Side = "Left" }),
+	MainSection4 = tabs.Main3:Section({ Side = "Left" }),
 }
 
 sections.MainSection1:Header({
 	Name = "Auto Kill"
+})
+sections.MainSection3:Header({
+	Name = "Player"
+})
+sections.MainSection4:Header({
+	Name = "Houses"
 })
 local plr = game.Players.LocalPlayer
 local plr3 = nil
@@ -56,13 +65,10 @@ sections.MainSection1:Button({
             local prevpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
             task.wait(0.05)
             for i = 0, 130 do
-                plr.Character.HumanoidRootPart.CFrame = plr2.Character.HumanoidRootPart.CFrame - Vector3.new(0, 1.5, 0)
+                plr.Character.HumanoidRootPart.CFrame = plr2.Character.PrimaryPart.CFrame + Vector3.new(0, -3.5, 0) 
                 task.wait()
             end
-            for i = 0, 200 do
-                plr.Character.HumanoidRootPart.CFrame = plr.Character.HumanoidRootPart.CFrame + Vector3.new(45.5, 1.5, 0)
-                task.wait()
-            end
+            plr.Character.HumanoidRootPart.CFrame = CFrame.new(100000, 25, 0)
             task.wait(0.80)
             workspace[name].Couch.Parent = plr.Backpack
             task.wait(0.3)
@@ -75,7 +81,7 @@ sections.MainSection1:Button({
 sections.MainSection2:Button({
 	Name = "Equip All Tools(Requires open tools menu)",
 	Callback = function()
-        for _, i in pairs(game:GetService("Players")["33asdsaddfssdaadsd"].PlayerGui.NoResetGUIHandler.MainToolMenu.Catalog.Container.ScrollingFrame:GetChildren()) do
+        for _, i in pairs(game.Players.LocalPlayer.PlayerGui.NoResetGUIHandler.MainToolMenu.Catalog.Container.ScrollingFrame:GetChildren()) do
             if i.Name ~= "UIGridLayout" then
                 game:GetService("ReplicatedStorage").RE["1Too1l"]:InvokeServer("PickingTools", i.Name)
                 task.wait()
@@ -85,5 +91,39 @@ sections.MainSection2:Button({
             i.Parent = workspace[plr.Name]
         end
         
+	end,
+})
+
+sections.MainSection3:Slider({
+	Name = "WalkSpeed",
+	Default = 20,
+	Minimum = 0,
+	Maximum = 500,
+	DisplayMethod = "Speed",
+	Precision = 0,
+	Callback = function(Value)
+	    plr.Character.Humanoid.WalkSpeed = Value
+	end
+}, "Slider")
+sections.MainSection3:Slider({
+	Name = "Jump Boost",
+	Default = 20,
+	Minimum = 0,
+	Maximum = 500,
+	DisplayMethod = "Boost",
+	Precision = 0,
+	Callback = function(Value)
+	    plr.Character.Humanoid.JumpPower = Value
+	end
+}, "Slider")
+
+sections.MainSection4:Button({
+	Name = "Anti Ban",
+	Callback = function()
+        for _, i in pairs(workspace:GetDescendants()) do
+		    if i.Name == "BannedBlock" or "PropBlocker13" and i:FindFirstChild("PropBlocker13") then
+		        i:Destroy()
+		    end
+		end
 	end,
 })
