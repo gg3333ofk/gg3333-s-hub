52048604b4a1ac6448be58b2f2cddbfa3d95ba8f
wanna_exploits <dragonwarhd@gmail.com>
2025-08-15 18:06:39 +0700

    Update brkhaven.lua

--- brkhaven.lua
+++ brkhaven.lua
@@ -1,7 +1,7 @@
 local MacLib = loadstring(game:HttpGet("https://github.com/biggaboy212/Maclib/releases/latest/download/maclib.txt"))()
 local Window = MacLib:Window({
 	Title = "Brookhaven Scripted",
-	Subtitle = "Version: Beta 1",
+	Subtitle = "Version: Beta 2",
 	Size = UDim2.fromOffset(868, 650),
 	DragStyle = 1,
 	DisabledWindowControls = {},
