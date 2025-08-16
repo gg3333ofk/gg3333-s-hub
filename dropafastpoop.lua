ec675cb326dfb76b53651eef25d6171b4cce8c4a
wanna_exploits <dragonwarhd@gmail.com>
2025-08-16 20:44:43 +0700

    Update dropafastpoop.lua

--- dropafastpoop.lua
+++ dropafastpoop.lua
@@ -5,12 +5,23 @@ local Section = Tab:NewSection("Exploits/Farming")
 local stata = nil
 local stata1 = nil
 local stata2 = nil
-
+local stata3 = nil
 Section:NewButton("Buy All", "buying luckyfood", function()
     for _, i in pairs(game.ReplicatedStorage.LuckyFoodModels:GetChildren()) do
         game.ReplicatedStorage.Network["LuckyFoodShop:RequestPurchase"]:InvokeServer(i.Name)
     end
 end)
+Section:NewToggle("Poop Exploit", "buying luckyfood", function(state)
+    stata3 = state
+    while task.wait() and stata3 do
+        game.ReplicatedStorage:FindFirstChild("PoopChargeStart"):FireServer()
+        for i = 0, 550 do
+            task.wait()
+            game.ReplicatedStorage.PoopEvent:FireServer(0)
+        end
+    end
+end)
+
 Section:NewToggle("Sell Exploit", "selling poop", function(state)
     stata2 = state
     while stata2 == true and task.wait(0.01) do
